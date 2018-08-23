a9f9416ea7fb6d57e294eb2c57a14b238bde49ab
Krzysztof Władyka <krzysztof@wladyka.eu>
2018-08-23 16:50:19 +0200

    add events tests; change update functions to new contract ver

--- src/district0x_tasks/server/contracts/district_tasks.cljs
+++ src/district0x_tasks/server/contracts/district_tasks.cljs
@@ -2,40 +2,25 @@
   (:require [district.server.smart-contracts :refer [contract-call]]
             [bignumber.core :as bn]))
 
-; tasks
+;;; tasks
 
 (defn add-task [title bidding-ends-on active? opts]
   "bidding-end-in is Unix Timestamp in seconds"
   (contract-call :district-tasks :add-task title bidding-ends-on active? opts))
 
-(defn count-tasks [opts]
-  (-> (contract-call :district-tasks :count-tasks opts)
-      (bn/number)))
+(defn update-task [task-id title bidding-ends-on active? opts]
+  (contract-call :district-tasks :update-task task-id title bidding-ends-on active? opts))
 
 (defn get-task [task-id opts]
   (let [[bidding-ends-on active?] (contract-call :district-tasks :tasks task-id opts)]
     {:bidding-ends-on (bn/number bidding-ends-on)
      :active? active?}))
 
-(defn update-task-active [task-id active? opts]
-  (contract-call :district-tasks :update-task-active
-                 task-id
-                 active?
-                 opts))
+(defn count-tasks [opts]
+  (-> (contract-call :district-tasks :count-tasks opts)
+      (bn/number)))
 
-(defn update-task-bidding-ends-on [task-id bidding-ends-on opts]
-  (contract-call :district-tasks :update-task-bidding-ends-on
-                 task-id
-                 bidding-ends-on
-                 opts))
-
-(defn update-task-title [task-id title opts]
-  (contract-call :district-tasks :update-task-title
-                 task-id
-                 title
-                 opts))
-
-; bids
+;;; bids
 
 (defn add-bid [task-id title description opts]
   (contract-call :district-tasks :add-bid
@@ -44,17 +29,11 @@
                  description
                  opts))
 
-(defn update-bid-title [task-id bid-id title opts]
-  (contract-call :district-tasks :update-bid-title
+(defn update-bid [task-id bid-id title description opts]
+  (contract-call :district-tasks :update-bid
                  task-id
                  bid-id
                  title
-                 opts))
-
-(defn update-bid-description [task-id bid-id description opts]
-  (contract-call :district-tasks :update-bid-description
-                 task-id
-                 bid-id
                  description
                  opts))
 
@@ -65,7 +44,7 @@
 (defn get-bid [task-id bid-id opts]
   {:creator (contract-call :district-tasks :get-bid task-id bid-id opts)})
 
-; voters
+;;; voters
 
 (defn add-voter [task-id bid-id opts]
   (contract-call :district-tasks :add-voter
@@ -82,3 +61,26 @@
 
 (defn voted? [task-id bid-id voter opts]
   (contract-call :district-tasks :is-voted task-id bid-id voter opts))
+
+;;; events
+
+(defn log-task->cljs [event]
+  (-> (update-in event [:args :id] bn/number)
+      (update-in [:args :bidding-ends-on] bn/number)))
+
+(defn log-bid->cljs [event]
+  (-> (update-in event [:args :task-id] bn/number)
+      (update-in [:args :bid-id] bn/number)))
+
+(defn log-add-voter->cljs [event]
+  (-> (update-in event [:args :task-id] bn/number)
+      (update-in [:args :bid-id] bn/number)))
+
+(defn event->cljs [event]
+  (let [convert (case (:event event)
+                  "LogAddTask" log-task->cljs
+                  "LogUpdateTask" log-task->cljs
+                  "LogAddBid" log-bid->cljs
+                  "LogUpdateBid" log-bid->cljs
+                  "LogAddVoter" log-add-voter->cljs)]
+    (convert event)))
--- test/district0x_tasks/contracts/tasks_test.cljs
+++ test/district0x_tasks/contracts/tasks_test.cljs
@@ -1,21 +1,27 @@
 (ns district0x-tasks.contracts.tasks-test
   (:require [cljs.test :refer-macros [deftest is testing run-tests use-fixtures async]]
+            [cljs.core.async :refer [<! timeout]]
             [cljs-web3.eth :as web3-eth]
             [bignumber.core :as bn]
+            [bignumber.js :as bnjs]
             [district.server.web3 :refer [web3]]
             [district.server.smart-contracts :refer [contract-call]]
             [district0x-tasks.server.dev :as dev]
             [district0x-tasks.server.deployer :as deployer]
             [district0x-tasks.server.contracts.district-tasks :as district-tasks]
-            [district.server.smart-contracts :refer [replay-past-events]]
+            [district.server.smart-contracts :refer [replay-past-events contract-event-in-tx]]
             [cljs-web3.evm :as web3-evm]))
 
 (dev/-main)
 
-(def accounts (web3-eth/accounts @web3))
-
 ;; {:from (first accounts)} is default account
 (deployer/deploy-tasks-contract! {})
+
+(def accounts (web3-eth/accounts @web3))
+
+(defn event->test [event]
+  (-> (district-tasks/event->cljs event)
+      (select-keys [:args :event])))
 
 ;; ganache-cli@6.1.8 block.timestamp is now instead of timestamp of last block
 (defn now-in-seconds []
@@ -52,9 +58,8 @@
   (let [bidding-ends-on (+ (now-in-seconds) 10)
         bidding-ends-on2 (+ bidding-ends-on 1)]
     (testing "Task update test"
-      (is (district-tasks/add-task "Update test" bidding-ends-on false {}))
-      (is (district-tasks/update-task-active 1 true {}))
-      (is (district-tasks/update-task-bidding-ends-on 1 bidding-ends-on2 {}))
+      (is (district-tasks/add-task "Title exist only in events" bidding-ends-on false {}))
+      (is (district-tasks/update-task 1 "New title" (+ bidding-ends-on 1) true {}))
       (is (= (district-tasks/get-task 1 {})
              {:bidding-ends-on bidding-ends-on2
               :active? true}))))
@@ -76,17 +81,66 @@
     (is (= (district-tasks/get-voters 0 0 {})
            [(first accounts)])))
 
-  (testing "BiddingEndsOn, Active testing"
-    (district-tasks/add-task "Title" (+ (now-in-seconds) 1) false {})
-    (is (thrown? js/Error
-                 (district-tasks/add-bid 2 "Bid title" "Bid description" {}))
-        "should not pass, because task is not active")
-    (district-tasks/update-task-active 2 true {})
-    ;; temporary solution, because ganache-cli block.timestamp is now instead of last block timestamp
-    (sleep 1)
-    ;; when block.timestamp will became last block timestamp
-    ;(web3-evm/increase-time! @web3 [1])
-    ;(web3-evm/mine! @web3)
-    (is (thrown? js/Error
-                 (district-tasks/add-bid 2 "Bid title" "Bid description" {}))
-        "should not pass, because BiddingEndsOn expired")))
+  (let [bidding-ends-on (+ (now-in-seconds) 1)]
+    (testing "BiddingEndsOn, Active testing"
+      (district-tasks/add-task "Title" bidding-ends-on false {})
+      (is (thrown? js/Error
+                   (district-tasks/add-bid 2 "Bid title" "Bid description" {}))
+          "should not pass, because task is not active")
+      (is (district-tasks/update-task 2 "Title" bidding-ends-on true {}))
+      ;; temporary solution, because ganache-cli block.timestamp is now instead of last block timestamp
+      (sleep 1)
+      ;; when block.timestamp will became last block timestamp
+      ;(web3-evm/increase-time! @web3 [1])
+      ;(web3-evm/mine! @web3)
+      (is (thrown? js/Error
+                   (district-tasks/add-bid 2 "Bid title" "Bid description" {}))
+          "should not pass, because BiddingEndsOn expired")))
+
+  (testing "events"
+    (let [bidding-ends-on (+ (now-in-seconds) 60)]
+      (is (= (-> (district-tasks/add-task "Event test" bidding-ends-on false {})
+                 (contract-event-in-tx :district-tasks :LogAddTask {})
+                 (event->test))
+             {:args {:id 3
+                     :title "Event test"
+                     :is-active false
+                     :bidding-ends-on bidding-ends-on}
+              :event "LogAddTask"})
+          "Add task")
+      (is (= (-> (district-tasks/update-task 0 "New title" bidding-ends-on true {})
+                 (contract-event-in-tx :district-tasks :LogUpdateTask {})
+                 (event->test))
+             {:args {:id 0
+                     :title "New title"
+                     :is-active true
+                     :bidding-ends-on bidding-ends-on}
+              :event "LogUpdateTask"})
+          "Update task")
+      (is (= (-> (district-tasks/add-bid 0 "Bid title" "Bid description" {})
+                 (contract-event-in-tx :district-tasks :LogAddBid {})
+                 (event->test))
+             {:args {:task-id 0
+                     :bid-id 1
+                     :title "Bid title"
+                     :description "Bid description"
+                     :creator (first accounts)}
+              :event "LogAddBid"})
+          "Add bid")
+      (is (= (-> (district-tasks/update-bid 0 1 "New bid title" "New bid description" {})
+                 (contract-event-in-tx :district-tasks :LogUpdateBid {})
+                 (event->test))
+             {:args {:task-id 0
+                     :bid-id 1
+                     :title "New bid title"
+                     :description "New bid description"}
+              :event "LogUpdateBid"})
+          "Update bid")
+      (is (= (-> (district-tasks/add-voter 0 1 {})
+                 (contract-event-in-tx :district-tasks :LogAddVoter {})
+                 (event->test))
+             {:args {:task-id 0
+                     :bid-id 1
+                     :voter (first accounts)}
+              :event "LogAddVoter"})
+          "Add voter"))))
