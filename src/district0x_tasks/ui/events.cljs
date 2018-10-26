072be026eb08552fbc27ab052c1f824ed2a87cbd
Krzysztof Władyka <krzysztof@wladyka.eu>
2018-10-26 11:34:19 +0200

    fix bug with empty active-account

--- src/district0x_tasks/ui/events.cljs
+++ src/district0x_tasks/ui/events.cljs
@@ -3,48 +3,25 @@
             [district.ui.web3-tx.events :as tx-events]
             [district.ui.smart-contracts.queries :as contract-queries]
             [district.ui.web3-accounts.queries :as account-queries]
+            [district.ui.logging.events :as logging]
             [district.ui.notification.events :as notification-events]))
 
 (re-frame/reg-event-fx
   ::add-bid
-  (fn [db [_ bid]]
+  (fn [{:keys [db]} [_ bid]]
     (println (pr-str bid))
     {}))
 
 (re-frame/reg-event-fx
   ::add-voter
-  (fn [db [_ bid]]
-    (println (pr-str (account-queries/active-account db)))
-    (println (pr-str (account-queries/accounts db)))
-    {}
-    #_{:dispatch [::tx-events/send-tx {:instance (contract-queries/instance db :district-tasks)
+  (fn [{:keys [db]} [_ bid]]
+    {:dispatch [::tx-events/send-tx {:instance (contract-queries/instance db :district-tasks)
                                      :fn :add-voter
                                      :args [(:task/id bid) (:bid/id bid)]
-                                     :tx-opts {:from (account-queries/active-account db)}}]}))
-
-;(dispatch [::tx-events/send-tx {:instance MintableToken
-;                               :fn :mint
-;                               :args [(first accounts) (web3/to-wei 1 :ether)]
-;                               :tx-opts {:from (first accounts) :gas 4500000}
-;                               :on-tx-hash [::tx-hash]
-;                               :on-tx-hash-n [[::tx-hash]]
-;                               :on-tx-hash-error [::tx-hash-error]
-;                               :on-tx-hash-error-n [[::tx-hash-error]]
-;                               :on-tx-success [::tx-success]
-;                               :on-tx-success-n [[::tx-success]]
-;                               :on-tx-error [::tx-error]
-;                               :on-tx-error-n [[::tx-error]]}])
-
-
-;{:dispatch [::tx-events/send-tx {:instance (contract-queries/instance db :DANK)
-;                                 :fn :approve-and-call
-;                                 :args [(contract-queries/contract-address db :meme-factory)
-;                                        deposit
-;                                        extra-data]
-;                                 :tx-opts {:from active-account
-;                                           :gas 6000000}
-;                                 :tx-id {:meme/create-meme tx-id}
-;                                 :on-tx-success-n [[::logging/success [::create-meme]]
-;                                                   [::notification-events/show (gstring/format "Meme created with meta hash %s" Hash)]]
-;                                 :on-tx-hash-error [::logging/error [::create-meme]]
-;                                 :on-tx-error [::logging/error [::create-meme]]}]})))
+                                     :tx-opts {:from (account-queries/active-account db)}
+                                     :on-tx-success-n [[::logging/success [::add-voter]]
+                                                       [::notification-events/show "Vote sent."]]
+                                     :on-tx-error-n [[::logging/error [::add-voter]]
+                                                     [::notification-events/show "Error during voting :("]]
+                                     :on-tx-hash-error-n [[::logging/error [::add-voter]]
+                                                          [::notification-events/show "Vote not sent to the network. You have to sign your vote by your key!"]]}]}))
