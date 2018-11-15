6b54085fe07ac09e0f3bdf6e46943d3206d41adb
Krzysztof Władyka <krzysztof@wladyka.eu>
2018-11-15 12:56:44 +0100

    fix core server default settings

--- src/district0x_tasks/server/core.cljs
+++ src/district0x_tasks/server/core.cljs
@@ -22,26 +22,25 @@
 
 (defn -main [& _]
   (-> (mount/with-args
-        {:config {:default {:web3 {:port 8545}}}
-         :smart-contracts {:contracts-var #'district0x-tasks.shared.smart-contracts/smart-contracts}
-         :graphql {:port 6500
-                   :middlewares [logging-middlewares]
-                   :schema (utils/build-schema graphql-schema
-                                               resolvers-map
-                                               {:kw->gql-name graphql-utils/kw->gql-name
-                                                :gql-name->kw graphql-utils/gql-name->kw})
-                   :field-resolver (utils/build-default-field-resolver graphql-utils/gql-name->kw)
-                   :path "/graphql"
-                   :graphiql true}
-         :web3-watcher {:on-online (fn []
-                                     (warn "Ethereum node went online again")
-                                     (mount/stop #'district0x-tasks.server.db/district0x-tasks-db)
-                                     (mount/start #'district0x-tasks.server.db/district0x-tasks-db
-                                                  #'district0x-tasks.server.syncer/syncer))
-                        :on-offline (fn []
-                                      (warn "Ethereum node went offline")
-                                      (mount/stop #'district0x-tasks.server.syncer/syncer))}
-         :syncer {:ipfs-config {:host "http://127.0.0.1:5001" :endpoint "/api/v0"}}})
+        {:config {:default {:web3 {:port 8545}}
+                  :smart-contracts {:contracts-var #'district0x-tasks.shared.smart-contracts/smart-contracts}
+                  :graphql {:port 6500
+                            :middlewares [logging-middlewares]
+                            :schema (utils/build-schema graphql-schema
+                                                        resolvers-map
+                                                        {:kw->gql-name graphql-utils/kw->gql-name
+                                                         :gql-name->kw graphql-utils/gql-name->kw})
+                            :field-resolver (utils/build-default-field-resolver graphql-utils/gql-name->kw)
+                            :path "/graphql"
+                            :graphiql true}
+                  :web3-watcher {:on-online (fn []
+                                              (warn "Ethereum node went online again")
+                                              (mount/stop #'district0x-tasks.server.db/district0x-tasks-db)
+                                              (mount/start #'district0x-tasks.server.db/district0x-tasks-db
+                                                           #'district0x-tasks.server.syncer/syncer))
+                                 :on-offline (fn []
+                                               (warn "Ethereum node went offline")
+                                               (mount/stop #'district0x-tasks.server.syncer/syncer))}}})
       (mount/except [#'district0x-tasks.server.deployer/deployer
                      #'district0x-tasks.server.generator/generator])
       (mount/start))
